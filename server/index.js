546ab52fd9245bd6d65c4dca7f106f377a352850
Pako Herrera <pakman_198@hotmail.com>
2019-11-21 12:54:53 -0800

    implemented passport to enable user authentication using facebook strategy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,28 @@
+
+require('dotenv').config({ path: '.env' });
 const express = require('express');
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook');
 
 const app = express();
+
+passport.use( new FacebookStrategy({
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: "/auth/facebook/callback"
+}, (accessToken, refreshToken, profile, done) => {
+  console.log({ accessToken, refreshToken, profile });
+}));
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    console.log({ req });
+    res.redirect('/');
+});
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there'})
@@ -8,5 +30,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 App running on port 5000 ${PORT}`);
+  console.log(`🚀 App running on port ${PORT}`);
 });
