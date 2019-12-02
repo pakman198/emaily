0e072e5574e69a452707362f8e1415742b4148a6
Pako Herrera <pakman_198@hotmail.com>
2019-12-02 10:26:38 -0800

    Added a client folder with the react application. Created a setupProxy file to handle all the links that are not part of the app router.

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,12 +7,12 @@ module.exports = (app) => {
     passport.authenticate('facebook', { failureRedirect: '/' }),
     function(req, res) {
       // Successful authentication, redirect home.
-      console.log({ req });
+      // console.log({ req });
       res.redirect('/');
   });
 
   app.get('/api/current_user', (req, res) => {
-    console.log("session", req.session)
+    // console.log("session", req.session)
     res.send(req.user);
   });
 
