59c403cca5b9b41cbd0ca5d699af79f686a20f94
Pako Herrera <pakman_198@hotmail.com>
2019-12-03 15:21:26 -0800

    Added meterialize-css for styling the application

---
+++ client/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <nav>
+    <div className="nav-wrapper">
+      <a href="#" className="brand-logo">Emaily</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="#">Login with Facebook</a></li>
+      </ul>
+    </div>
+  </nav>
+  )
+}
+
+export default Header;
