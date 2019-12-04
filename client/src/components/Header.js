7381f7d95afc8f2a21ebe9c15db43a54f7d65226
Pako Herrera <pakman_198@hotmail.com>
2019-12-04 11:10:01 -0800

    Used redux hook to connect the Header component to the store, that way some conditional content is displayed

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,40 @@
 import React from 'react';
+import { useSelector } from 'react-redux'
 
 const Header = () => {
+  // this hook is like the mapStateToProps param on the connect
+  const auth = useSelector(state => state.auth);
+
+  console.log({ auth })
+
+  const renderContent = (auth) => {
+    switch(auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/facebook">Login with Facebook</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        )
+    }
+  }
+
   return (
     <nav>
-    <div className="nav-wrapper">
-      <a href="/" className="brand-logo">Emaily</a>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="/auth/facebook">Login with Facebook</a></li>
-      </ul>
-    </div>
-  </nav>
+      <div className="nav-wrapper">
+        <a href="/" className="brand-logo">Emaily</a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          { renderContent(auth) }
+        </ul>
+      </div>
+    </nav>
   )
 }
 
