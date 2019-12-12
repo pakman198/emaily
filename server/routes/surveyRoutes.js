a6315afcaef8fafd95093b24223b2df76abcba3f
Pako Herrera <pakman_198@hotmail.com>
2019-12-12 14:53:01 -0800

    Created a webhook since sendgrid sends notifications to the server regarding the activity on the emails sent. The purpose of the webhook is to update the values of the survey (register the clients response). Once the user has voted, wont be able to vote again (the event wont be updated on the db)

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const { Path } = require('path-parser');
+const { URL } = require('url');
+const _ = require('lodash');
  
 const authentication = require('../middlewares/authentication');
 const validateCredits = require('../middlewares/validateCredits');
@@ -8,11 +11,6 @@ const surveyTemplate = require('../services/emailTemplates/survey');
 const Survey = mongoose.model('Survey')
 
 module.exports = app => {
-
-  app.get('/api/surveys/thanks', (req,res) => {
-    res.send('Thanks for voting!');
-  });
-
   app.post('/api/surveys', authentication, validateCredits, async (req, res) => {
     const { title, body, subject, recipients } = req.body;
     const survey = new Survey({
@@ -36,7 +34,45 @@ module.exports = app => {
       console.log({ err });
       res.status(422).send(err)
     }
+  });
 
+  app.get('/api/surveys/:surveyID/:choice', (req,res) => {
+    res.send('Thanks for voting!');
+  });
 
+  app.post('/api/surveys/webhooks', (req,res) => {
+    const p = new Path('/api/surveys/:surveyID/:choice');
+    const events = _.chain(req.body)
+      .map(event => {
+        const match = p.test(new URL(event.url).pathname);
+
+        if(match) {
+          return {
+            email: event.email,
+            surveyID: match.surveyID,
+            choice: match.choice
+          }
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyID')
+      .each(event => {
+        Survey.findOneAndUpdate({
+          _id: event.surveyID,
+          recipients: {
+            $elemMatch: { email: event.email, responded: false }
+          }
+        }, {
+          $inc: { [event.choice]: 1 },
+          $set: { 'recipients.$.responded': true },
+          lastResponded: new Date()
+        }).exec()
+      })
+      .value();
+    
+
+    console.log({ events })
+
+    res.send({});
   });
 }
--- server/services/emailTemplates/survey.js
+++ server/services/emailTemplates/survey.js
@@ -8,10 +8,10 @@ module.exports = (survey) => {
         <p>Please answer the following question:</p>
         <p>${survey.body}</p>
         <div>
-          <a href="${process.env.REDIRECT_DOMAIN}/api/survesy/thanks">Yes</a>
+          <a href="${process.env.REDIRECT_DOMAIN}/api/surveys/${survey.id}/yes">Yes</a>
         </div>
         <div>
-          <a href="${process.env.REDIRECT_DOMAIN}/api/survesy/thanks">No</a>
+          <a href="${process.env.REDIRECT_DOMAIN}/api/surveys/${survey.id}/no">No</a>
         </div>
       </div>
     </body>
